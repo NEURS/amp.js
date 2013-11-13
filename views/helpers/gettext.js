7ed579b0287d8cb0dc4391fc5aa8b0ab838eeb75
Kevin Martin <kevfirst101+git@gmail.com>
2013-11-13 14:06:04 -0500

    Make sure to use Gettext locale

--- views/helpers/gettext.js
+++ views/helpers/gettext.js
@@ -1,7 +1,6 @@
 var amp		= require('../../utils/base'),
 	fs		= require('fs'),
-	Gettext	= require('node-gettext'),
-	gettext	= new Gettext();
+	Gettext	= require('node-gettext')
 	
 String.prototype.format = function () {
 	Array.prototype.unshift.call(arguments, this);
@@ -11,25 +10,26 @@ String.prototype.format = function () {
 
 module.exports = amp.Class.extend({
 	_gettext: null,
-	_locale: null,
 
 	init: function (request, data) {
-		this.locale	= (request.accept &&
+		var locale = request.language || (request.accept &&
 			request.accept.languages &&
 			request.accept.languages.getBestMatch(amp.config.L10n.supported)) ||
 			amp.config.L10n.supported[0];
+
+		this._gettext = new Gettext(locale);
 	},
 
 	set locale(value) {
 		if (amp.config.L10n.supported.indexOf(value) > -1) {
-			this._locale = value;
+			this._gettext.set(value);
 		} else {
 			console.log('Locale "'.red + value + '" not supported. Please update your core configuration file in APP/config/core.js'.red);
 		}
 	},
 
 	get locale() {
-		return this._locale || amp.config.L10n.supported[0];
+		return this._gettext.get() || amp.config.L10n.supported[0];
 	},
 
 	gettext: function (msgid) {
