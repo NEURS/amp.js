1b2f3623cf369c4944f33e38bd105178e2e686d7
Kevin Martin <kevfirst101+git@gmail.com>
2013-11-13 14:09:58 -0500

    Revert and add user defined request.language

--- views/helpers/gettext.js
+++ views/helpers/gettext.js
@@ -1,6 +1,7 @@
 var amp		= require('../../utils/base'),
 	fs		= require('fs'),
-	Gettext	= require('node-gettext')
+	Gettext	= require('node-gettext'),
+	gettext = new Gettext();
 	
 String.prototype.format = function () {
 	Array.prototype.unshift.call(arguments, this);
@@ -9,27 +10,25 @@ String.prototype.format = function () {
 };
 
 module.exports = amp.Class.extend({
-	_gettext: null,
+	_locale: null,
 
 	init: function (request, data) {
-		var locale = request.language || (request.accept &&
+		this._locale = request.language || (request.accept &&
 			request.accept.languages &&
 			request.accept.languages.getBestMatch(amp.config.L10n.supported)) ||
 			amp.config.L10n.supported[0];
-
-		this._gettext = new Gettext(locale);
 	},
 
 	set locale(value) {
 		if (amp.config.L10n.supported.indexOf(value) > -1) {
-			this._gettext.set(value);
+			this._locale = value;
 		} else {
 			console.log('Locale "'.red + value + '" not supported. Please update your core configuration file in APP/config/core.js'.red);
 		}
 	},
 
 	get locale() {
-		return this._gettext.get() || amp.config.L10n.supported[0];
+		return this._locale || amp.config.L10n.supported[0];
 	},
 
 	gettext: function (msgid) {
